99096ef3481d0d870e4987956010de27d225e33f
David Capella <D_Capella@yahoo.com>
2017-07-25 16:47:21 -0500

    modified kele.gemspec to have runtime with JSON and add gemfile to run the dependencies and modified lib/kele.rb to allow the function .get_me to retrieve own user data

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,24 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
+    ### Retrieve and storing auth token when passing valid credentials and raising error otherwise
+
     response = self.class.post('/sessions', body: { email: "#{email}", password: "#{password}" } )
     @auth_token = response["auth_token"]
     raise "ERROR: Wrong email or password." if @auth_token.nil?
+  end
+
+  def get_me
+    ### Retrieve the current user from the Bloc API and converting into a ruby hash
+
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    document = JSON[response]
+    document = JSON.parse(document)
+    document = JSON.parse(document)
   end
 end
